86a87bc5d77f46a89620af5ddf92ab35d4d07a48
Pranjal Gupta <pranjalg2308@gmail.com>
2020-11-04 17:06:51 +0530

    font incomplete code

--- src/ui/font-builder.js
+++ src/ui/font-builder.js
@@ -1,7 +1,51 @@
 
+const MSDFShader = require('three-bmfont-text/shaders/msdf')
+const createGeometry = require('three-bmfont-text');
+const loadFont = require('load-bmfont');
+const fontFile = require("../../font/Roboto-msdf.json");
+const fontAtlas = require("../../font/Roboto-msdf.png");
 
 export default class FontBuilder{
-    constructor(text){
+    constructor(text, scene){
+        // pos is vec2 location for middle of the node 
         this.text = text
+        this.scene = scene
+        this.loadBMF();
+
+    }
+
+    loadBMF(){
+        // Create geometry of packed glyphs
+        loadFont(fontFile, (err, font)=>{
+            this.geometry = createGeometry({
+                font, 
+                text:this.vars.word
+            })
+        })
+
+        // Load texture containing font glyphs
+        this.loader = new THREE.TextureLoader();
+        this.loader.load(fontAtlas, texture => {
+        setTimeout(() => {
+            this.init(this.geometry, texture);
+            // this.animate();
+        }, 1500);
+        });
+    }
+
+    init(geometry, texture) {
+        this.createMesh(geometry, texture);
+        this.onResize();
+        window.addEventListener("resize", () => this.onResize(), false);
+        this.render();
+    }
+
+    getMesh(geometry, material) {
+        const mesh = new THREE.Mesh(geometry, material);
+
+        mesh.castShadow = true;
+        mesh.receiveShadow = true;
+
+        return mesh;
     }
 }
--- src/ui/nodeui.js
+++ src/ui/nodeui.js
@@ -1,8 +1,24 @@
 //node UI threejs 
-
+var MSDFShader = require('three-bmfont-text/shaders/msdf')
+var createGeometry = require('three-bmfont-text');
+var loadFont = require('load-bmfont');
 
 export default class NodeUI{
 
-    
+    constructor(text, scene, pos){
+        // pos is vec2 location for middle of the node 
+        this.text = text
+        this.scene = scene
+        this.scene.add()
+    }
+
+    getMesh(geometry, material) {
+        const mesh = new THREE.Mesh(geometry, material);
+
+        mesh.castShadow = true;
+        mesh.receiveShadow = true;
+
+        return mesh;
+    }
 
 }
